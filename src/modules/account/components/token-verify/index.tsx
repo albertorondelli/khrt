c6b60692684df059047b7930cad2b98621376cd2
Riccardo Rondelli <riccardo.rondelli0@gmail.com>
2024-01-23 11:08:29 +0100

    added medusaFetch to use with custom api

--- src/modules/account/components/token-verify/index.tsx
+++ src/modules/account/components/token-verify/index.tsx
@@ -1,6 +1,7 @@
+import medusaRequest from "@lib/medusa-fetch"
 import { Button } from "@medusajs/ui"
 import { LOGIN_VIEW } from "@modules/account/templates/login-template"
-import Spinner from "@modules/common/icons/spinner"
+import { CheckMini } from "@medusajs/icons"
 import Link from "next/link"
 import { useRouter, useSearchParams } from "next/navigation"
 import { useEffect, useState } from "react"
@@ -9,69 +10,65 @@ type Props = {
   setCurrentView: (view: LOGIN_VIEW) => void
 }
 
-const VerifyToken = ({setCurrentView}: Props) => {
-  const [loading, setLoading] = useState(true)
-
+const VerifyToken = ({ setCurrentView }: Props) => {
   const router = useRouter()
-
-
 
   const searchParams = useSearchParams()
   const token = searchParams.get("token-verify") as string
 
   const [verify, setVerify] = useState(false)
+  const [loading, setLoading] = useState(false)
 
-  // useEffect(() => {
-  //   const verifyToken = async (token: string) => {
-  //     setLoading(true)
-  //     try {
-  //       medusaRequest("GET", `/token-verify/${token}`).then((res) => {
-  //         if (res.ok) {
-  //           setVerify(true)
-  //         } else {
-  //           setVerify(false)
-  //         }
-  //       })
-  //     } catch (e) {
-  //       console.log(e)
-  //     }
-  //     setLoading(false)
-  //   }
+  useEffect(() => {
+    const verifyToken = async (token: string) => {
+      setLoading(true)
+      try {
+        medusaRequest("GET", `/token-verify/${token}`).then((res) => {
+          if (res.ok) {
+            setVerify(true)
+          } else {
+            setVerify(false)
+          }
+        })
+      } catch (e) {
+        console.log(e)
+      }
+      setLoading(false)
+    }
 
-  //   verifyToken(token)
-  // }, [])
+    verifyToken(token)
+  }, [])
 
   const handleOnClick = async () => {
+    // Remove the query from the url
     const currentUrl = window.location.href
     const url = new URL(currentUrl)
+    
     url.search = "" // Clear all query parameters
     router.replace(url.toString())
-
-    // Wait for the router to finish navigating before executing the next actions
-    await new Promise((resolve) => setTimeout(resolve, 250))
 
     setCurrentView(LOGIN_VIEW.SIGN_IN)
   }
 
   return (
     <div className="max-w-sm flex flex-col items-center">
-      {loading ? (
-        <div className="z-10 fixed inset-0 bg-white bg-opacity-50 flex items-center justify-center">
-          <Spinner size={24} />
-        </div>
-      ) : verify ? (
-        <div>
-          <h1 className="text-large-semi text-center uppercase mb-6">
+      {verify ? (
+        <>
+          <h1 className="flex text-large-semi text-center uppercase mb-6 me-2">
+            <CheckMini className="me-2" />
             Email successfully verified
           </h1>
+
           <p className="text-base-regular text-gray-700 mb-8">
             Your email has been successfully verified. You can now log in with
             your email and password
           </p>
-          <div className="pt-4">
-            <Button onClick={handleOnClick}>Login</Button>
+          <div className="pt-4 w-full">
+            <Button className="w-full" size="large" onClick={handleOnClick}>
+              Sign in
+            </Button>
           </div>
-        </div>
+        </>
       ) : (
         <div>
           <h1 className="text-large-semi text-center uppercase mb-6">
@@ -83,7 +80,9 @@ const VerifyToken = ({setCurrentView}: Props) => {
           </p>
           <div className="pt-4">
             <Link href="/customer-service" passHref>
-              <Button className="uppercase">Customer service</Button>
+              <Button className="w-full" variant="secondary" size="large">
+                Customer service
+              </Button>
             </Link>
           </div>
         </div>
