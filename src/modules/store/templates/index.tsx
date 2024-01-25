76a2bc02e143ac1057e6a4e0f4728dd7f3e12e59
Riccardo Rondelli <riccardo.rondelli0@gmail.com>
2024-01-25 10:46:04 +0100

    added meilisearch

--- src/modules/store/templates/index.tsx
+++ src/modules/store/templates/index.tsx
@@ -1,26 +1,37 @@
-"use client"
-
 import InfiniteHits from "@modules/products/components/infinite-hits"
 import { SEARCH_INDEX_NAME, searchClient } from "@lib/search-client"
-import { InstantSearch } from "react-instantsearch"
+import {
+  InstantSearch,
+  InstantSearchSSRProvider,
+  InstantSearchServerState,
+} from "react-instantsearch"
 import MeilisearchRefinementList from "../components/meilisearch-refinement-list"
 
-export default function Store() {
+type StoreProps = {
+  serverState?: InstantSearchServerState
+}
+
+export default function Store(serverState: StoreProps) {
   return (
     <div>
-      <InstantSearch indexName={SEARCH_INDEX_NAME} searchClient={searchClient}>
-        <div className="flex flex-col small:flex-row small:items-start py-6">
-          <div className="px-2 w-60">
-            {/* <MeilisearchRefinementList /> */}
-          </div>
-          <div className="flex-1 w-full">
-            <div className="content-container w-full mb-4 px-3 text-2xl-semi">
-              <h1>Store</h1>
+      <InstantSearchSSRProvider {...serverState}>
+        <InstantSearch
+          indexName={SEARCH_INDEX_NAME}
+          searchClient={searchClient}
+        >
+          <div className="flex flex-col small:flex-row small:items-start py-6">
+            <div className="px-2 w-60">
+              {/* <MeilisearchRefinementList /> */}
             </div>
-            <InfiniteHits />
+            <div className="flex-1 w-full">
+              <div className="content-container w-full mb-4 px-3 text-2xl-semi">
+                <h1>Store</h1>
+              </div>
+              <InfiniteHits />
+            </div>
           </div>
-        </div>
-      </InstantSearch>
+        </InstantSearch>
+      </InstantSearchSSRProvider>
     </div>
   )
 }
