a537c933c249d7e5bfadd1f64b95b35946ce38f7
alby85 <alby85@gmail.com>
2024-02-29 15:00:35 +0100

    added cookiescript

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
       <head>
-        <script async id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="90575a33-b118-451b-8d07-a0c08a6010be" data-blockingmode="auto" type="text/javascript"></script>
+      <script async type="text/javascript" src="//cdn.cookie-script.com/s/4616f9ced57c5c7377c5f25c13f8180b.js"></script>
       </head>
       <body>
         <main className="relative">{props.children}</main>
