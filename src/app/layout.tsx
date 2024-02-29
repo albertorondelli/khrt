3a5f44a6d9eb6cd8a4487c9225e31babbeaf75ef
alby85 <alby85@gmail.com>
2024-02-29 14:24:50 +0100

    added cookiebot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -10,6 +10,9 @@ export const metadata: Metadata = {
 export default function RootLayout(props: { children: React.ReactNode }) {
   return (
     <html lang="en" data-mode="light">
+      <head>
+        <script async id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="90575a33-b118-451b-8d07-a0c08a6010be" data-blockingmode="auto" type="text/javascript"></script>
+      </head>
       <body>
         <main className="relative">{props.children}</main>
       </body>
