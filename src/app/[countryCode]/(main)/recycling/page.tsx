e02415fcb5ac6f29b070cdfc808bc1298d4a0d7e
Riccardo Rondelli <riccardo.rondelli0@gmail.com>
2025-04-04 16:43:36 +0200

    update: added recycling page + translation

---
+++ src/app/[countryCode]/(main)/recycling/page.tsx
@@ -0,0 +1,40 @@
+import { Table } from "@medusajs/ui"
+import { Tag } from "@medusajs/icons"
+import { useTranslation } from "@i18n/client"
+
+export default function Recycling() {
+  const { t } = useTranslation("recycling-and-sustainability")
+
+  return (
+    <div className="content-container w-full space-y-8 h-[80dvh] mb-4 px-3 py-6">
+      <h1 className="text-2xl-semi uppercase">{t("recycling")}</h1>
+      <div className="space-y-2">
+        <p>{t("instructions")}</p>
+        <p>{t("qr-code")}</p>
+
+        <p>{t("guide")}</p>
+        <p>{t("check-disposal")}</p>
+      </div>
+      <Table>
+        <Table.Header>
+          <Table.Row>
+            <Table.HeaderCell>{t("reference-image")}</Table.HeaderCell>
+            <Table.HeaderCell>{t("material-description")}</Table.HeaderCell>
+            <Table.HeaderCell>{t("material-type")}</Table.HeaderCell>
+            <Table.HeaderCell>{t("how-to-recycle")}</Table.HeaderCell>
+          </Table.Row>
+        </Table.Header>
+        <Table.Body>
+          <Table.Row>
+            <Table.Cell>
+              <Tag />
+            </Table.Cell>
+            <Table.Cell>{t("paper-tag")}</Table.Cell>
+            <Table.Cell>{t("paper")}</Table.Cell>
+            <Table.Cell>{t("pap21")}</Table.Cell>
+          </Table.Row>
+        </Table.Body>
+      </Table>
+    </div>
+  )
+}
